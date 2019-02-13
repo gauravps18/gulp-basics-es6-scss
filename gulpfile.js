0c8ea280aaec605cb4bd772f965d829b08484a4c
Gaurav Sali <gauravps18@gmail.com>
2019-02-13 20:42:02 +0530

    WIP: browserify and babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
+const  babelify = require('babelify');
 const uglify = require('gulp-uglify');
+const browserify = require('browserify');
 
 
 gulp.task('uglify', () =>
@@ -15,10 +16,23 @@ gulp.task('transpile', () =>
         }))
 )
 
+gulp.task('scripts', () =>
+    browserify({
+        entries: ['src/js/app.js']
+    })
+    .transform(babelify, {
+        presets: ['@babel/preset-env']
+    })
+    .bundle()
+    //.pipe(uglify())
+    .pipe(gulp.dest('dist'))
+)
+
 gulp.task('default', () =>
     gulp.src('src/js/**/*.js')
         .pipe(babel({
             presets: ['@babel/env']
         }))
+        .pipe(uglify())
         .pipe(gulp.dest('dist'))
 )
