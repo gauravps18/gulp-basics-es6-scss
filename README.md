bb0fa3a22904bc42882e37f2f7208b54256a77ab
Gaurav Sali <gauravps18@gmail.com>
2019-02-14 18:08:28 +0530

    feat: js budling for counter app.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # gulp-basics-es6-scss
-Gulp basics with ES6 and SASS
+Gulp basics with ES6, browserify, babelify and SASS
+
+# Getting Started
+Install Dependencies
+
+```
+npm install
+```
+
+Gulp CLI
+
+```
+npm install gulp-cli -g
+```
+
+Run Server
+```
+gulp serve
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,48 @@
 const gulp = require('gulp');
-const  babelify = require('babelify');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const babelify = require('babelify');
 const uglify = require('gulp-uglify');
 const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const del = require('del');
+const webserver = require('gulp-webserver');
 
 
-gulp.task('uglify', () =>
-    gulp.src('src/js/**/*.js')
-        .pipe(uglify())
+gulp.task('clean', () =>
+    del('dist/**', {force:true})
 )
 
-gulp.task('transpile', () =>
-    gulp.src('src/js/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-)
-
-gulp.task('scripts', () =>
-    browserify({
-        entries: ['src/js/app.js']
-    })
+gulp.task('scripts', gulp.series('clean', () =>
+    browserify('src/js/app.js')
     .transform(babelify, {
-        presets: ['@babel/preset-env']
+        "presets": ["env"]
     })
     .bundle()
-    //.pipe(uglify())
+    .pipe(source('bundle.js'))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'))
-)
+))
 
-gulp.task('default', () =>
-    gulp.src('src/js/**/*.js')
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(uglify())
+gulp.task('index', () =>
+    gulp.src('src/index.html')
         .pipe(gulp.dest('dist'))
 )
+
+gulp.task('build', gulp.series('scripts', 'index'))
+
+gulp.task('serve', gulp.series('build', () =>
+    gulp.src('dist')
+        .pipe(webserver({
+            livereload: true,
+            directoryListening: true,
+            open: true
+        }))
+))
+
+gulp.task('default', gulp.series('build'))
